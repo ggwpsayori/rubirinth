70cc477135b5baffa1bfbbf691452ecba8bf97bc
ggwpsayori <ggwpsayori@gmail.com>
2026-08-30 16:09:09 +0300

    fix(ui): fix action button width calculation and corner clipping in server cards

--- packages/ui/src/components/project/card/ProjectCard.vue
+++ packages/ui/src/components/project/card/ProjectCard.vue
@@ -96,7 +96,7 @@
 					</div>
 
 					<!-- Full-width bottom action bar -->
-					<div class="pt-1 flex items-center justify-center w-full smart-clickable:allow-pointer-events [&>div]:w-full [&>div]:items-center [&>button]:w-full [&>button]:justify-center [&_button.grow]:w-full [&_button.grow]:justify-center">
+					<div class="pt-1 flex items-center justify-center w-full smart-clickable:allow-pointer-events [&>div]:w-full [&>div]:items-center [&>button]:w-full [&>button]:justify-center">
 						<slot name="actions" />
 					</div>
 				</div>
